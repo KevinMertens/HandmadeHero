ed679abbcd33db8d3f13198b56b03e18b8644390
Kevin Mertens <kevin_mertens@telenet.be>
2016-03-08 20:53:50 +0100

    Day 01

---
+++ HandmadeHero/win32_handmade.cpp
@@ -0,0 +1,7 @@
+#include <Windows.h>
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	MessageBox(0, "Handmade Hero", "This is handmade hero", MB_OK | MB_ICONINFORMATION);
+	return(0);
+}
