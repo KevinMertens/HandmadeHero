5e2d2738fd4e8910e3b620257dcd7d48c0cfccb0
Kevin Mertens <kevin_mertens@telenet.be>
2016-03-09 16:32:46 +0100

    Day 002

--- HandmadeHero/win32_handmade.cpp
+++ HandmadeHero/win32_handmade.cpp
@@ -1,7 +1,86 @@
 #include <Windows.h>
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
-	MessageBox(0, "Handmade Hero", "This is handmade hero", MB_OK | MB_ICONINFORMATION);
+LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam) {
+	LRESULT Result = 0;
+	switch (Message)
+	{
+		case WM_SIZE:
+		{
+			OutputDebugStringA("WM_SIZE\n");
+			break;
+		}
+		case WM_DESTROY:
+		{
+			OutputDebugStringA("WM_DESTROY\n");
+			break;
+		}
+		case WM_CLOSE:
+		{
+			OutputDebugStringA("WM_CLOSE\n");
+			break;
+		}
+		case WM_ACTIVATEAPP:
+		{
+			OutputDebugStringA("WM_ACTIVATEAPP\n");
+			break;
+		}
+		case WM_PAINT:
+		{
+			PAINTSTRUCT Paint;
+			HDC DeviceContext = BeginPaint(Window, &Paint);
+			int x = Paint.rcPaint.left;
+			int y = Paint.rcPaint.top;
+			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+			static DWORD Operation = WHITENESS;
+			PatBlt(DeviceContext, x, y, Width, Height, Operation);
+			EndPaint(Window, &Paint);
+			if (Operation == WHITENESS) {
+				Operation = BLACKNESS;
+			}
+			else {
+				Operation = WHITENESS;
+			}
+			break;
+		}
+		default:
+		{
+			//OutputDebugString("Default\n");
+			Result = DefWindowProc(Window, Message, WParam, LParam);
+			break;
+		}
+	}
+	return(Result);
+}
+
+
+int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommanddLine, int ShowCode) {
+	WNDCLASS WindowClass = {};
+	WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+	WindowClass.lpfnWndProc = MainWindowCallback;
+	WindowClass.hInstance = Instance;
+	WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+	if (RegisterClass(&WindowClass)) {
+		HWND WindowHandle = CreateWindowEx(0, WindowClass.lpszClassName, "Handmade Hero", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, Instance, 0);
+		if (WindowHandle) {
+			MSG Message;
+			for (;;) {
+				BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+				if(MessageResult > 0) {
+					TranslateMessage(&Message);
+					DispatchMessage(&Message);
+				}
+				else {
+					break;
+				}
+			}
+		}
+		else {
+			//TODO
+		}
+	}
+	else {
+		//TODO
+	}
 	return(0);
 }
